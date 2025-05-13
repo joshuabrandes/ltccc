cca1ddfb8f1db6906a5223de9849b56ef4d50a90
Joshua Brandes <47532061+joshuabrandes@users.noreply.github.com>
2025-05-13 21:10:50 +0200

    trying to get into Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::os::raw::{c_void, c_uint};
+
+pub struct Cache {
+    // TODO: Felder
+    capacity: usize,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[no_mangle]
+pub extern "C" fn cache_init(capacity: c_uint) -> *mut Cache {
+    let cache = Cache { capacity: capacity as usize };
+    Box::into_raw(Box::new(cache)) as *mut Cache
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+/// releases the memory of the cache
+#[no_mangle]
+pub extern "C" fn cache_destroy(ptr: *mut Cache) {
+    if ptr.is_null() {
+        return;
     }
+    unsafe {
+        let _ = Box::from_raw(ptr);
+    }
+}
+
+/// example function
+#[no_mangle]
+pub extern "C" fn cache_example(ptr: *mut Cache, key: c_uint, value: c_uint) {
+    let cache = unsafe {
+        assert!(!ptr.is_null());
+        &mut *ptr
+    };
+    // TODO: implement actual functionality
+    println!(
+        "Setting key={} to value={} in cache with capacity={}",
+        key, value, cache.capacity
+    );
 }
